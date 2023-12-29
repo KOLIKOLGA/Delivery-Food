f4e1d52fc525ec66587758d9cb1a806e6af3c2f9
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2023-12-29 07:11:48 +0300

    add restsArray[]

--- scripts/index.js
+++ scripts/index.js
@@ -30,11 +30,57 @@ const restFunc = () => {
   const restArray = [
     {
       id: 0,
-      title: "",
-      time: 0,
-      rating: 0,
-      price: 0,
-      type: "",
+      title: "Пицца плюс",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "image1.jpg",
+    },
+    {
+      id: 1,
+      title: "Тануки",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "image2.jpg",
+    },
+    {
+      id: 2,
+      title: "FoodBand",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "image3.jpg",
+    },
+    {
+      id: 3,
+      title: "Жадина-пицца",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "image4.jpg",
+    },
+    {
+      id: 4,
+      title: "Точка еды",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "image5.jpg",
+    },
+    {
+      id: 5,
+      title: " PizzaBurger",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "image6.jpg",
     },
   ];
 
@@ -42,7 +88,48 @@ const restFunc = () => {
     container.innerHTML =
       "<p style = 'width : 100%; text-align : center;'>загрузка</p>";
   };
-  loading();
+
+  const renderRests = (array) => {
+    container.innerHTML = "";
+
+    array.forEach((card) => {
+      container.insertAdjacentHTML(
+        "beforeend",
+        `
+        <a href="./goods.html?id='${card.id}'" class="products-card">
+        <div class="products-card__image">
+          <img src="./images/rests/${card.image}" alt="rests1" />
+        </div>
+        <div class="products-card__description">
+          <div class="products-card__description-row">
+            <h4 class="products-card__description-title">${card.title}</h4>
+            <div class="products-card__description-bage">${card.time} мин</div>
+          </div>
+          <div class="products-card__description-row">
+            <div class="products-card__description-info">
+              <div class="products-card__description-info--rating">
+                <img src="./images/icon/star.svg" alt="star" />${card.rating}
+              </div>
+              <div class="products-card__description-info--price">
+                От ${card.price} ₽
+              </div>
+              <div class="products-card__description-info--group">
+              ${card.type}
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      `
+      );
+    });
+  };
+  if (container) {
+    loading();
+    setTimeout(() => {
+      renderRests(restArray);
+    }, 2000);
+  }
 };
 
 modalFunc();
